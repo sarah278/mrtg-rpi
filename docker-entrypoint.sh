f92f676b7be9f7fc81a4e401b364d06fd00f6e28
Gerry Gosselin <ggosselin@gmail.com>
2016-12-09 21:18:13 -0500

    making more readable, fixing permission issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,14 @@ grep SG300 /usr/bin/cfgmaker || \
 	sed -i '/my $descr = $routers->{$router}{deviceinfo}{sysDescr};/a push @Variables, "ifAlias" if $descr =~ /SG300/;' /usr/bin/cfgmaker
 
 /usr/local/bin/generate-mrtg-config.sh 192.168.8.1 public
-echo "3 * * * * /usr/local/bin/generate-mrtg-config.sh 192.168.8.1 public" >> /tmp/crontab
-
 /usr/local/bin/generate-mrtg-config.sh 192.168.2.2 public
-echo "3 * * * * /usr/local/bin/generate-mrtg-config.sh 192.168.2.2 public" >> /tmp/crontab
 
 indexmaker /home/mrtg/mrtg.cfg --output /home/mrtg/data/index.html
+chown -R mrtg:nogroup /home/mrtg
 
-
+echo "3 * * * * /usr/local/bin/generate-mrtg-config.sh 192.168.8.1 public" >> /tmp/crontab
+echo "7 * * * * /usr/local/bin/generate-mrtg-config.sh 192.168.2.2 public" >> /tmp/crontab
 echo "0,5,10,15,20,25,30,35,40,45,50,55 * * * * /usr/bin/mrtg /home/mrtg/mrtg.cfg --logging /home/mrtg/logs/mrtg.log" >> /tmp/crontab
-cat /tmp/crontab | crontab -u mrtg -
 
+cat /tmp/crontab | crontab -u mrtg -
 exec crond -l 2 -f
