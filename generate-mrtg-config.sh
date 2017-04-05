46d0693cc7c19153cae2632ff262c8a3e2b1df18
Gerry Gosselin <ggosselin@gmail.com>
2017-04-05 16:27:45 -0400

    using logscale

--- generate-mrtg-config.sh
+++ generate-mrtg-config.sh
@@ -3,7 +3,7 @@ set -x
 
 cfgmaker \
 	--global "WorkDir: /home/mrtg/data" \
-	--global 'Options[_]: bits,growright' \
+	--global 'Options[_]: bits,growright,logscale' \
 	--output /home/mrtg/cfg/$1.mrtg.cfg \
 	--ifref=name \
 	--ifdesc=alias \
