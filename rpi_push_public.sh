c126cee854766a0bd364c6dcf51fc0d309261111
Gerry Gosselin <ggosselin@gmail.com>
2017-04-11 20:26:45 -0400

    adding timezone support, cleaning up public push

--- rpi_push_public.sh
+++ rpi_push_public.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -f Dockerfile.rpi -t ggosselin/mrtg:rpi .
 docker push ggosselin/mrtg:rpi
+docker rmi ggosselin/mrtg:rpi
--- x86_push_public.sh
+++ x86_push_public.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -f Dockerfile.x86 -t ggosselin/mrtg .
 docker push ggosselin/mrtg
+docker rmi ggosselin/mrtg
